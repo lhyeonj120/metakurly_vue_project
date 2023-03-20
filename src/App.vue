75ee61c2041b0c57366ea50ee0848fbc3ba17c21
lhyeonj120 <isk04387@gmail.com>
2023-03-20 15:50:31 +0900

    style: add Main Page nav

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link class="navbar-brand" :to="{name: 'Home'}">Home</router-link>
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-iem active">
+        <router-link class="nav-link" :to="{name: 'Best'}">Best</router-link>
+      </li>
+      <li class="nav-iem active">
+        <router-link class="nav-link" :to="{name: 'Product'}">Product</router-link>
+      </li>
+      <li class="nav-iem active">
+        <router-link class="nav-link" :to="{name: 'myPage'}">myPage</router-link>
+      </li>
+    </ul>
+  </nav>
+  <div class="container">
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
